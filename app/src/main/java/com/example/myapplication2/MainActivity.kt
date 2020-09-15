c904a1f29b6120894061124ba5d48b44e7e25c92
yuyanchong <384788283@qq.com>
2020-09-15 23:29:25 +0800

    随意修改

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
+        //哈哈哈哈
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
